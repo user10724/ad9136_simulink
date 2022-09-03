21c90ab85c5935ce3bbe97595540d32ec325f654
user10724 <1072462276@qq.com>
2022-09-04 00:18:20 +0800

    master

---
+++ AD9136/slblocks.m
@@ -0,0 +1,13 @@
+function blkStruct = slblocks		
+        % This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'ad9136';
+		% 'mylib' is the name of the library
+
+		Browser.Name = 'AD9136';
+		% 'My Library' is the library name that appears 
+             % in the Library Browser
+
+		blkStruct.Browser = Browser; 
